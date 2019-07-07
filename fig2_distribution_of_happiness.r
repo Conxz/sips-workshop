bab91c69a721a6529574d661f32e249ed9c40139
Conxz <bnucon@gmail.com>
2019-07-07 15:00:07 +0200

    organize research folder3

--- fig2_distribution_of_happiness.r
+++
@@ -1,7 +0,0 @@
-data <- read.csv("data/data.csv", header=TRUE, sep=",", as.is=TRUE)
-
-dev.new()
-png("results/fig2_distribution_of_happiness.png")
-boxplot(happiness_rating ~ trade_number, data = data)
-dev.off()
-
